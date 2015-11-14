a53d1385a246c71d1131218e7bcae67472233ada
carynova <carychan317@gmail.com>
2015-11-14 15:57:29 +0800

    fb3-1~4

---
+++ flex_and_bison/c3/fb3-1.h
@@ -0,0 +1,27 @@
+// 声明
+
+// 与词法分析器的接口
+extern int yylineno;
+void yyerror(char *s, ...);
+
+// 抽象语法树中的节点
+struct ast {
+  int nodetype;
+  struct ast *l;
+  struct ast *r;
+};
+
+struct numval {
+  int nodetype;
+  double number;
+};
+
+// 构造抽象语法树
+struct ast* newast(int nodetype, struct ast *l, struct ast *r);
+struct ast* newnum(double d);
+
+// 计算抽象语法树
+double eval(struct ast*);
+
+// 删除和释放抽象语法树
+void treefree(struct ast*);
